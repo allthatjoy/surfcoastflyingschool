043013431881b1666d7c1100abe110d95c9c5945
allthatjoy <allthatjoy@gmail.com>
2024-05-27 22:23:54 +0800

    update content

--- src/config/nav.js
+++ src/config/nav.js
@@ -3,14 +3,10 @@ export const nav = [
 		title: 'Home',
 		slug: '/',
 	},
-	{
-		title: 'Blog',
-		slug: '/blog',
-	},
-	{
-		title: 'About',
-		slug: '/company/about',
-	},
+	// {
+	// 	title: 'About',
+	// 	slug: '/company/about',
+	// },
 	{
 		title: 'Contact',
 		slug: '/company/contact',
