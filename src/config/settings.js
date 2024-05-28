4917192455aa34b9f96d9b97fff92168f96437e6
allthatjoy <allthatjoy@gmail.com>
2024-05-28 16:49:39 +0800

    modified favicon

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-  enableThemeSwitcher: true,
+	title: `Surf Coast Flying School`,
+	description: `A flying school based in Barwon Heads.`,
+	url: `https://surfcoastflyingschool.com.au`, // No trailing slash!
+	name: `Surf Coast Flying School`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  enableThemeSwitcher: false,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
